dbd41159c7d39bf64dde376bbda4e28700b64038
Marzouq Adebayo <marzouqaadebayo@gmail.com>
2024-05-20 17:13:52 +0000

    Fixed user_todo undefined bug

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -12,18 +12,18 @@ if __name__ == "__main__":
     todo_path = f'{endpoint}/user/{user_id}/todos'
     user_path = f'{endpoint}/users/{user_id}'
     todo_path_output = get(todo_path).json()
-    user_path_output = get(todo_user).json()
+    user_path_output = get(user_path).json()
 
     user_id = user_path_output.get("id")
-    user_name = user_path_output.get("name")
+    user_name = user_path_output.get("username")
     user_tasks_list = []
     for item in todo_path_output:
         user_task_object = {}
         user_task_object.update({"user_ID": user_id,
                                  "username": user_name,
                                  "completed": item.get("completed"),
-                                 "task": todo.get("title")})
-        user_tasks_list.append(user_tasks_object)
+                                 "task": item.get("title")})
+        user_tasks_list.append(user_task_object)
     with open("{}.csv".format(user_id), 'w', newline='') as f:
         header = ["user_ID", "username", "completed", "task"]
         writer = DictWriter(f, fieldnames=header, quoting=QUOTE_ALL)
