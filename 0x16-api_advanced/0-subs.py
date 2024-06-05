72d6c1a2abfb58aee52035601332d8bd9da73715
Marzouq Adebayo <marzouqaadebayo@gmail.com>
2024-06-05 01:20:06 +0000

    Committing file 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,7 +9,7 @@ def number_of_subscribers(subreddit):
     10 hot posts listed for a given subreddit.
     """
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'My User Agent 1.0'}
+    headers = {'User-Agent': 'Some User Agent 4.0'}
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
         data = response.json()
