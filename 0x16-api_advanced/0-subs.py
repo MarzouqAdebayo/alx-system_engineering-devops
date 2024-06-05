717f2cb9f23214379bf8dddd7c2198b4976a38f8
Marzouq Adebayo <marzouqaadebayo@gmail.com>
2024-06-05 01:12:15 +0000

    Committing file 0-subs.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""module '0-subs.py' fetch data from the reddit api"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    function that queries the Reddit API and prints the titles of the first
+    10 hot posts listed for a given subreddit.
+    """
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'My User Agent 1.0'}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        return data.get('data').get('subscribers')
+    else:
+        return 0
