ac9eedae92e51846eacddffe941ccd9e67e501bb
Marzouq Adebayo <marzouqaadebayo@gmail.com>
2024-06-05 01:12:17 +0000

    Committing file 1-top_ten.py

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""module '1-top_ten.py' to fetch from reddit api"""
+import requests
+
+
+def top_ten(subreddit):
+    """
+    function that queries the Reddit API and prints the titles of the first
+    10 hot posts listed for a given subreddit.
+    """
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    headers = {'User-Agent': 'Chrome/1.0;Mozilla/5.0'}
+    try:
+        response = requests.get(url, headers=headers,
+                                allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            children = data.get('data').get('children')
+            print(children)
+            for i in range(10):
+                print(children[i].get('data').get('title'))
+        else:
+            print("None")
+    except Exception:
+        print("None")
