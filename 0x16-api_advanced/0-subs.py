9a7f9d31c1f73957bba46429d1a102e8d4b0344d
MarzouqAdebayo <marzouqaadebayo@gmail.com>
2024-06-23 01:59:08 +0100

    Testing again 3

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -13,7 +13,6 @@ def number_of_subscribers(subreddit):
                             .format(subreddit),
                             headers={"User-Agent": "Some-User-Agent-4.0"},
                             allow_redirects=False)
-    if (response.status_code >= 300:
+    if (response.status_code >= 300):
         return 0
     return response.json().get("data").get("subscribers")
-        return 0
