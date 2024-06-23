0c05bd12ded3c18ed2361edabe43afb92aeef2fa
MarzouqAdebayo <marzouqaadebayo@gmail.com>
2024-06-23 02:15:36 +0100

    Fix 2

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """module '1-top_ten.py' to fetch from reddit api"""
-import requests
 
 
 def top_ten(subreddit):
     """function that queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit."""
+    import requests
+
+
     url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
     headers = {'User-Agent': 'Chrome/1.0;Mozilla/5.0'}
     response = requests.get(url, headers=headers,
