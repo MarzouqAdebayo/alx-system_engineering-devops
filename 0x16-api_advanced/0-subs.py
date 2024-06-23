effc785bd74a6bc35b9b41087d4a8f0d7b43a2f9
MarzouqAdebayo <marzouqaadebayo@gmail.com>
2024-06-23 01:50:00 +0100

    Testing again 1

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """module "0-subs.py" fetch data from the reddit api"""
-import requests
 
 
 def number_of_subscribers(subreddit):
@@ -8,6 +7,8 @@ def number_of_subscribers(subreddit):
     function that queries the Reddit API and prints the titles of the first
     10 hot posts listed for a given subreddit.
     """
+    import requests
+
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Some-User-Agent-4.0"}
     response = requests.get(url, headers=headers,
