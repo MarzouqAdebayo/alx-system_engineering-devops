09e54df5d38bc766a0a8311c7c5219efb92282a8
MarzouqAdebayo <marzouqaadebayo@gmail.com>
2024-06-23 02:13:47 +0100

    Fix 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,16 +7,13 @@ def top_ten(subreddit):
     """function that queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit."""
     url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
     headers = {'User-Agent': 'Chrome/1.0;Mozilla/5.0'}
-    try:
-        response = requests.get(url, headers=headers,
-                                allow_redirects=False)
-        if response.status_code == 200:
-            data = response.json()
-            children = data.get('data').get('children')
-            for item in children:
-                print(item.get('data').get('title'))
-        else:
-            print("None")
-    except Exception:
+    response = requests.get(url, headers=headers,
+                            allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        children = data.get('data').get('children')
+        for item in children:
+            print(item.get('data').get('title'))
+    else:
         print("None")
 
