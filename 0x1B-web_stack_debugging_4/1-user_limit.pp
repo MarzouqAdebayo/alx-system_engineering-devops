4c3d0bf9f30fc083dd10bfc14ac5cb97406a9718
MarzouqAdebayo <marzouqaadebayo@gmail.com>
2024-08-25 05:32:07 +0100

    Completed web debugging

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,4 @@
+# Permanently increases the maximum number of open file descriptors to 20000 by editing `/etc/security/limits.conf`
+exec { 'Fix hard limit':
+  command => '/usr/bin/env sed -i "s/4/20000/; s/5/20000/" /etc/security/limits.conf'
+}
